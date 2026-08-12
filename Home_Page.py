d941f63e9a0bc52649a96796ea5daa467c676145
colleenxyu <colleenelise.yu@gmail.com>
2026-08-12 23:36:44 +0800

    Add files via upload

--- Home_Page.py
+++ Home_Page.py
@@ -44,10 +44,11 @@ def dashboard():
     st.sidebar.header("Dashboard Menu")
 
     #Sidebar Details
-    #st.sidebar.page_link ("./Home_Page.py", label="Home Page")
+    st.sidebar.page_link ("Home_Page.py", label="Home Page")
     st.sidebar.page_link("pages/Current_Year_Dashboard.py", label="Current Year Dashboard")
     st.sidebar.page_link("pages/Year_On_Year_Dashboard.py", label="Year On Year Dashboard")
     st.sidebar.page_link("pages/Historical_Dashboard.py", label="Historical Dashboard")
+    st.sidebar.page_link("pages/Utilities_Dashboard.py", label="Utilities Dashboard")
 
     st.sidebar.button("Logout", on_click=logout)
 
